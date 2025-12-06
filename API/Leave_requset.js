6669e415c28e73b75e61390c430e44745c488536
rohankosti <kostirohan186@gmail.com>
2025-12-07 01:32:30 +0530

    complate leave request modal

--- API/Leave_requset.js
+++ API/Leave_requset.js
@@ -7,7 +7,7 @@ const leavereq = (req, res, dbs) => {
   let body = "";
   req.on("data", (chunk) => {
     body += chunk.toString();
-    console.log(body);
+    // console.log(body);
   });
 
   req.on("end", async () => {
@@ -51,7 +51,7 @@ const updatedleave = (req, res, dbs) => {
     body += chunk.toString();
   });
   req.on("end", async () => {
-    console.log(body);
+    // console.log(body);
     const parse = JSON.parse(body);
     const updated = await dbs.collection("LeaveRequset").updateOne(
       { _id: new ObjectId(parse.id) },
@@ -66,9 +66,39 @@ const updatedleave = (req, res, dbs) => {
       res.end(JSON.stringify({ msg: "Leave Request Updated Sucssesfully" }));
     } else {
       res.writeHead(200, { "content-type": "application/json" });
-      res.end(JSON.stringify({ msg1: "Leave Request Can't be updated ",
-        msg: "Leave Request Updated Sucssesfully",
-       }));
+      res.end(
+        JSON.stringify({
+          msg1: "Leave Request Can't be updated ",
+          msg: "Leave Request Updated Sucssesfully",
+        })
+      );
+    }
+  });
+};
+
+const delateleaverequest = (req, res, dbs) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+
+  req.on("end", async () => {
+    // console.log(body);
+    const rejparse = JSON.parse(body);
+    // console.log(rejparse);
+    const rejcollection = await dbs
+      .collection("LeaveRequset")
+      .deleteOne({ _id: new ObjectId(rejparse.id) });
+    if (rejcollection.deletedCount === 1) {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ msg: "Leave Request Reject Succsesfully" }));
+    } else {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          msg1: "Leave Request Can't be Rejected ",
+        })
+      );
     }
   });
 };
@@ -78,6 +108,7 @@ const leaverequest = {
   getleavereq,
   singleleaverequest,
   updatedleave,
+  delateleaverequest,
 };
 
 export default leaverequest;
